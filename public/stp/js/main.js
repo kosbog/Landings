5639ed22c6ea433adc17d28122d942da7551c508
Bogdan Kositskiy <kosbog@MacBook-Pro-Bogdan.local>
2018-01-24 18:05:35 +0200

    wip

---
+++ public/stp/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(document).ready(function () {
+
+    $('.mail-form').on('submit', function (e) {
+        e.preventDefault();
+        wl_shared.registerUser($('.email').val());
+    });
+
+    $('.email').focus(function () {
+        $('.email-error').text('');
+        $('.email').removeClass('js-mail-visible');
+    });
+
+    window.uloginAuthCb = function (token) {};
+});
+//# sourceMappingURL=main.js.map
